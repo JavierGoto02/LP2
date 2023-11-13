a74fa057f40f80dae4a4feb776b6d9057e57be2b
dai1dai <fabrizziokawabata@gmail.com>
2023-11-13 11:29:00 -0300

    Mejorar abonarSaldo
    Se ha mejorado el funcionamiento del método abonarSaldo agregando nuevos atributos fechaLimitePago e interesPenalizacion

--- Simulador_Grupo_1/src/Clases/TarjetaCredito.java
+++ Simulador_Grupo_1/src/Clases/TarjetaCredito.java
@@ -1,31 +1,31 @@
 package Clases;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
  * @author Grupo 1
  */
-public abstract class TarjetaCredito extends Tarjeta
-{
-    
+public abstract class TarjetaCredito extends Tarjeta {
+
     private int saldoPendiente;
-    
-    
-    
+
     private int pagoMinimo;
-    private int limiteCredito;
+    private int lineaCredito;
     private float interes;
+    private float interesPenalizacion;
+    private Date fechaLimitePago;
 
-
-
-
-    public TarjetaCredito(int nroTarjeta, Date fechaVencimiento, int cVC, int idCuenta, int pagoMinimo, int limiteCredito, float interes) {
+    public TarjetaCredito(int nroTarjeta, Date fechaVencimiento, int cVC, int idCuenta, int pagoMinimo,
+            int lineaCredito, float interes, float interesPenalizacion, Date fechaLimitePago) {
         super(nroTarjeta, fechaVencimiento, cVC, idCuenta);
         this.saldoPendiente = 0;
         this.pagoMinimo = pagoMinimo;
-        this.limiteCredito = limiteCredito;
+        this.lineaCredito = lineaCredito;
         this.interes = interes;
+        this.interesPenalizacion = interesPenalizacion;
+        this.fechaLimitePago = fechaLimitePago;
     }
 
     public int getPagoMinimo() {
@@ -36,8 +36,7 @@ public abstract class TarjetaCredito extends Tarjeta
         this.pagoMinimo = pagoMinimo;
     }
 
-    public int getSaldoPendiente()
-    {
+    public int getSaldoPendiente() {
         return saldoPendiente;
     }
 
@@ -49,30 +48,50 @@ public abstract class TarjetaCredito extends Tarjeta
         this.interes = interes;
     }
 
+    public float getInteresPenalizacion() {
+        return interesPenalizacion;
+    }
+
+    public void setInteresPenalizacion(float interesPenalizacion) {
+        this.interesPenalizacion = interesPenalizacion;
+    }
+
+    public Date getFechaLimitePago() {
+        return fechaLimitePago;
+    }
+
+    public void setFechaLimitePago(Date fechaLimitePago) {
+        this.fechaLimitePago = fechaLimitePago;
+    }
+
     @Override
     public int consultarSaldo() {
-        return limiteCredito - saldoPendiente;
+        return lineaCredito - saldoPendiente;
     }
 
     @Override
-    public void realizarPago(int monto) 
-    {
-        saldoPendiente += monto;  
+    public void realizarPago(int monto) {
+        saldoPendiente += monto;
     }
 
     @Override
-    public void abonarSaldo(int monto) 
-    {
-        //TO DO: Hacer funcional el interes
-        if(monto >= pagoMinimo)
-            saldoPendiente -= monto; 
+    public void abonarSaldo(int monto) {
+
+        if (monto >= pagoMinimo) {
+            // Se define la fecha actual como la fecha de pago 
+            Date fechaPago = new Date();
+            // Si se pagó a los días posteriores al fecha límite, se aplicará un interés diario de penalización
+            if (fechaLimitePago.compareTo(fechaPago) < 0) {
+                long diferenciaTiempo = fechaPago.getTime() - fechaLimitePago.getTime();
+                long diferenciaDia = TimeUnit.DAYS.convert(diferenciaTiempo, TimeUnit.MILLISECONDS);
+                for (int i = 0; i < diferenciaDia; i++) {
+                    saldoPendiente += saldoPendiente + saldoPendiente * interesPenalizacion;
+                }
+            }
+            saldoPendiente -= monto;
+            // El resto del importe a pagar genera interés
+            saldoPendiente += saldoPendiente + saldoPendiente * interes;
+            fechaLimitePago.setMonth(fechaLimitePago.getMonth() + 1);
+        }
     }
-
-    
-
-    
-
-
-    
-    
 }
